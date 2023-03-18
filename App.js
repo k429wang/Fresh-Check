aa09175644da69a0793edc2a5d6342fb137760ef
Kai Wang <kdwang0917@gmail.com>
2023-03-18 12:14:41 -0400

    containerization

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Landing } from './containers';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi cindy</Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( 
+    <Landing/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
